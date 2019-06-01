f796efca232815d625bc2955705130956f3e57bf
Denis Bazhenov <bazhenov@farpost.com>
2019-06-01 14:00:42 +1000

    debugging facilities

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include "lvs302.h"
+
+int main(void) {
+	lvs302 acc;
+	int i;
+	rcc_periph_clock_enable(RCC_GPIOD);
+	gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
+
+	while (1) {
+		gpio_toggle(GPIOD, GPIO12);
+		for (i = 0; i < 800000; i++)
+			__asm__("nop");
+	}
+	return 0;
+}
